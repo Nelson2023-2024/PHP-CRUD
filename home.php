49124f726b9d249df47995bc06e1ec09ec279a11
Nelson Obuya Ouya <nelsonobuya19@gmail.com>
2024-01-09 15:51:23 +0300

    Fetching the data from the DB and displaying them on the page

--- home.php
+++ home.php
@@ -28,19 +28,38 @@
             <tbody>
                 <?php 
                 require('mysqli_connect.php');
-                ?>
+
+                //read from DB
+                //NB - If your SQL query does not have any parameters, you don't need to use mysqli_stmt_bind_param at all.
+                $select_query = "SELECT * FROM clients";
+                $select_stmt = mysqli_stmt_init($dbcon);
+                mysqli_stmt_prepare($select_stmt, $select_query);
+                mysqli_stmt_execute($select_stmt);
+
+                $result = mysqli_stmt_get_result($select_stmt);
+
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "
                 <tr>
-                    <td>ID</td>
-                    <td>Bill Gates</td>
-                    <td>bill@microsoft.com</td>
-                    <td>+254115720771</td>
-                    <td>New York, USA</td>
-                    <td>09/01/2024</td>
+                    <td>$row[id]</td>
+                    <td>$row[name]</td>
+                    <td>$row[email]</td>
+                    <td>$row[phone]</td>
+                    <td>$row[address]</td>
+                    <td>$row[created_at]</td>
                     <td>
-                        <a href="edit.php" class="btn btn-primary">Edit</a>
-                        <a href="delete.php" class="btn btn-danger">Delete</a>
+                        <a href='edit.php?id=$row[id]' class='btn btn-primary'>Edit</a>
+                        <a href='delete.php?id=$row[id]' class='btn btn-danger'>Delete</a>
                     </td>
                 </tr>
+                    ";
+
+                }
+                
+
+                ?>
+                
+                
             </tbody>
         </table>
     </div>
